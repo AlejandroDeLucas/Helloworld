d82372ddae88a153dd7d9891789c8c8ba12e45ab
AlejandroDeLucas <73598103+AlejandroDeLucas@users.noreply.github.com>
2026-03-24 21:24:22 +0100

    Fix clear-path check to ignore self hits

---
+++ Assets/Scripts/Core/Input/IGameInputSource.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+namespace TinyHunter.Core.Input
+{
+    public interface IGameInputSource
+    {
+        Vector2 Move { get; }
+        Vector2 Look { get; }
+
+        bool JumpPressed { get; }
+        bool CrouchTogglePressed { get; }
+        bool DodgePressed { get; }
+        bool SprintHeld { get; }
+        bool InteractPressed { get; }
+        bool PrimaryAttackPressed { get; }
+        bool GuardHeld { get; }
+        bool LockTogglePressed { get; }
+        bool InventoryTogglePressed { get; }
+        bool EscapePressed { get; }
+        bool SavePressed { get; }
+        bool LoadPressed { get; }
+    }
+}
